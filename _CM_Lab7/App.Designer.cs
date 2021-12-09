3f72d945e72a0cb8a2a6817491af3475f40e1281
БЕЗДОТНая Хоопс ✶ ДоскаИгры <vnifedow@bk.ru>
2021-12-09 18:26:27 +0500

    Добавьте файлы проекта.

---
+++ _CM_Lab7/App.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace _CM_Lab7
+{
+    partial class App
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Start = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart.Legends.Add(legend3);
+            this.chart.Location = new System.Drawing.Point(12, 12);
+            this.chart.Name = "chart";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart.Series.Add(series3);
+            this.chart.Size = new System.Drawing.Size(776, 378);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(1001, 110);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(185, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Начать";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13});
+            this.dataGridView.Location = new System.Drawing.Point(12, 396);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(1174, 166);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "";
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Сумма";
+            this.Column13.Name = "Column13";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(794, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 121);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(1001, 55);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 4;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(794, 139);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(392, 251);
+            this.listBox.TabIndex = 5;
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 577);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.chart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "App";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ _CM_Lab7/App.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _CM_Lab7
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+            chart.Series.Clear();
+        }
+
+        List<double> x = new List<double>()
+        {
+            0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1
+        };
+
+        List<double> y = new List<double>() 
+        {
+            18.422, 12.844, 9.659, 7.394, 5.670, 4.303, 3.196, 2.297, 1.572, 1.000, 0.569
+        };
+        
+        IEnumerable<double> Squared()
+        {
+            for(int i = 0; i < x.Count; i++)
+            {
+                yield return Math.Pow(x[i], 2);
+            }
+        }
+
+        IEnumerable<double> Mul()
+        {
+            for (int i = 0; i < x.Count; i++)
+            {
+                yield return x[i] * y[i];
+            }
+        }
+
+        List<double> Cramer(List<List<double>> Mtrx, List<double> Equal)
+        {
+            double FindDet(List<List<double>> Mtr) 
+            => Mtr[0][0] * Mtr[1][1] - Mtr[0][1] * Mtr[1][0];
+            if (FindDet(Mtrx) != 0) // Проверка на совместимость с методом
+            {
+                return new List<double>
+                {
+                    FindDet(new List<List<double>> // x
+                    {
+                        new List<double> { Equal[0],  Mtrx[0][1] },
+                        new List<double> { Equal[1],  Mtrx[1][1] }
+                    }),
+                    FindDet(new List<List<double>> // y
+                    {
+                        new List<double> { Mtrx[0][0],  Equal[0] },
+                        new List<double> { Mtrx[1][0],  Equal[1] }
+                    })
+                };
+            } 
+            else
+            {
+                return null;
+            }
+        }
+
+        double PreferedValue;
+        string PreferedString;
+
+        void BuildLinear(double k, double b)
+        {
+            listBox.Items.Add("/// Линейный ///");
+            chart.Series.Add(new Series("Линейная") { ChartType = SeriesChartType.Line });
+            listBox.Items.Add($"Линейное уравнение: y = {k}x + ({b})");
+            List<double> Array = new List<double>();
+            for(int i = 0; i < x.Count; i++)
+            {
+                chart.Series["Линейная"].Points.AddXY(x[i], k * x[i] + b);
+                Array.Add(k * x[i] + b);
+            }
+            PreferedValue = ToShitDeltaY(Array);
+            PreferedString = "линейный";
+            listBox.Items.Add($"Зигма: {ToShitDeltaY(Array)}");
+        }
+
+        double FindM()
+        {
+            double Sum1() 
+            {
+                double sum = 0;
+                for(int i = 0; i < x.Count; i++)
+                {
+                    sum += Math.Log(x[i]) * Math.Log(y[i]);
+                }
+                return sum;
+            }
+            double Sum2()
+            {
+                double sum = 0;
+                for (int i = 0; i < x.Count; i++)
+                {
+                    sum += Math.Log(x[i]);
+                }
+                return sum;
+            }
+            double Sum3()
+            {
+                double sum = 0;
+                for (int i = 0; i < x.Count; i++)
+                {
+                    sum += Math.Log(y[i]);
+                }
+                return sum;
+            }
+            double Sum4()
+            {
+                double sum = 0;
+                for (int i = 0; i < x.Count; i++)
+                {
+                    sum += Math.Pow(Math.Log(x[i]), 2);
+                }
+                return sum;
+            }
+            return (x.Count * Sum1() - Sum2() * Sum3()) / (Sum4() - Math.Pow(Sum2(), 2));
+        }
+
+        double FindC(double m)
+        {
+            double Sum1()
+            {
+                double sum = 0;
+                for(int i = 0; i < x.Count; i++)
+                {
+                    sum += Math.Log(y[i]);
+                }
+                return sum;
+            }
+            double Sum2()
+            {
+                double sum = 0;
+                for (int i = 0; i < x.Count; i++)
+                {
+                    sum += Math.Log(x[i]);
+                }
+                return sum;
+            }
+            return Math.Exp(/*(1 / x.Count)*/0.09 * (Sum1() - m * Sum2()));
+        }
+
+        double ToShitDeltaY(List<double> Array)
+        {
+            double min = Math.Abs(Array[1] - Array[0]);
+            for(int i = 1; i < Array.Count; i++)
+            {
+                if(min > Math.Abs(Array[i] - Array[i - 1]))
+                {
+                    min = Math.Abs(Array[i] - Array[i - 1]);
+                }
+            }
+            return min;
+        }
+
+        void BuildDegree(double sumx, double sumyx, double sumsqx)
+        {
+            listBox.Items.Add("/// Степенной ///");
+            chart.Series.Add(new Series("Степенная") { ChartType = SeriesChartType.Line });
+            double m = FindM();
+            double c = FindC(m);
+            List<double> Array = new List<double>();
+            listBox.Items.Add($"Значение c: {c}");
+            listBox.Items.Add($"Значение m: {m}");
+            listBox.Items.Add($"Степенное уравнение: y = {c}x^{m}");
+            for(int i = 0; i < x.Count; i++)
+            {
+                chart.Series["Степенная"].Points.AddXY(x[i], c * Math.Pow(x[i], m));
+                Array.Add(c * Math.Pow(x[i], m));
+            }
+            if(PreferedValue > ToShitDeltaY(Array))
+            {
+                PreferedString = "степенной";
+            }
+            listBox.Items.Add($"Зигма: {ToShitDeltaY(Array)}");
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            chart.Series.Add(new Series("Исходная") { ChartType = SeriesChartType.Line });
+            chart.Series["Исходная"].Points.DataBindXY(x, y);
+            List<double> Sum = new List<double>() { 0, 0, 0, 0 };
+            dataGridView.Rows.Add("x");
+            dataGridView.Rows.Add("y");
+            dataGridView.Rows.Add("Sqr(x)");
+            dataGridView.Rows.Add("xy");
+            for(int i = 0; i < x.Count; i++)
+            {
+                dataGridView.Rows[0].Cells[i + 1].Value = x[i];
+                Sum[0] += x[i];
+                dataGridView.Rows[1].Cells[i + 1].Value = y[i];
+                Sum[1] += y[i];
+                dataGridView.Rows[2].Cells[i + 1].Value = Squared().ToArray()[i];
+                Sum[2] += Squared().ToArray()[i];
+                dataGridView.Rows[3].Cells[i + 1].Value = Mul().ToArray()[i];
+                Sum[3] += Mul().ToArray()[i];
+            }
+            dataGridView.Rows[0].Cells[12].Value = Sum[0];
+            dataGridView.Rows[1].Cells[12].Value = Sum[1];
+            dataGridView.Rows[2].Cells[12].Value = Sum[2];
+            dataGridView.Rows[3].Cells[12].Value = Sum[3];
+            label.Text = $"{Sum[2]}k + {Sum[0]}b = {Sum[3]}\n{Sum[0]}k + {x.Count}b = {Sum[1]}";
+            listBox.Items.Add("Решение методом Крамера...");
+            double k = Cramer(new List<List<double>> { new List<double> { Sum[2], Sum[0] }, new List<double> { Sum[0], x.Count } }, new List<double> { Sum[3], Sum[1] })[0];
+            double b = Cramer(new List<List<double>> { new List<double> { Sum[2], Sum[0] }, new List<double> { Sum[0], x.Count } }, new List<double> { Sum[3], Sum[1] })[1];
+            listBox.Items.Add($"Значение k: {k}");
+            listBox.Items.Add($"Значение b: {b}");
+            BuildLinear(k, b);
+            BuildDegree(Sum[0], Sum[3], Sum[2]);
+            listBox.Items.Add($"По результатам зигмы побеждает {PreferedString} метод!");
+            Start.Visible = false;
+        }
+    }
+}
